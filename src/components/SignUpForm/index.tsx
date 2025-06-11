682e930c3dacd23081089e527a785273f8defaca
Wenceslao <wences_gv@hotmail.com>
2025-06-11 17:28:02 -0600

    Basic Sign up component.

--- src/components/SignUpForm/index.tsx
+++ src/components/SignUpForm/index.tsx
@@ -1,27 +1,38 @@
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+import { Label } from '@radix-ui/react-label';
 import { useState, type ComponentProps, type FormEvent } from 'react';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
 
-export function LoginForm({ className, ...props }: ComponentProps<'form'>) {
+export function SignUpForm({ className, ...props }: ComponentProps<'form'>) {
+  const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
   const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    /*try {
-      await login({ email, password });
-      console.log('Login successful from form');
+    const userData = {
+      name: name,
+      email: email,
+      password: password
+    };
+    /*
+    try {
+      await register(userData);
+      console.log('Registration successful from form, user is now logged in.');
     } catch (err) {
-      console.error('Login failed in form:', err);
+      console.error('Registration failed in form:', err);
     }*/
   };
-
   return (
     <form className="p-6 md:p-8" onSubmit={handleSubmit} {...props}>
       <div className="flex flex-col gap-6">
         <div className="flex flex-col items-center text-center">
-          <h1 className="text-2xl font-bold">Welcome back</h1>
-          <p className="text-muted-foreground text-balance">Login to your Acme Inc account</p>
+          <h1 className="text-2xl font-bold">Welcome!</h1>
+          <p className="text-muted-foreground text-balance">Create your Acme Inc account</p>
+        </div>
+        <div className="grid gap-3">
+          <Label htmlFor="text">Name</Label>
+          <Input id="name" type="text" placeholder="John Doe" required value={name} onChange={(e) => setName(e.target.value)} />
         </div>
         <div className="grid gap-3">
           <Label htmlFor="email">Email</Label>
@@ -34,7 +45,7 @@ export function LoginForm({ className, ...props }: ComponentProps<'form'>) {
           <Input id="password" type="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
         </div>
         <Button type="submit" className="w-full">
-          Login
+          Sign up
         </Button>
       </div>
     </form>
